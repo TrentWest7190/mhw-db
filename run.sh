811463239ea03bf21ca615482ccb6596614f5bab
Trent West <trentwest7190@yahoo.com>
2018-03-03 17:54:52 -0600

    more scripting

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-psql -f ./generate_database.sql
+psql -d monster_hunter_world -f ./generate_database.sql
+
+cat headers.csv weapons/*.csv | psql -d monster_hunter_world -c 'COPY weapons FROM stdin CSV HEADER'
